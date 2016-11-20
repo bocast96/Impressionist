950bb61718cfb1f18cfa3041a1d760fb454bb622
Boris Castillo <bocast96@terpmail.umd.edu>
2016-11-20 00:36:14 -0500

    First Commit

---
+++ app/src/main/java/edu/umd/hcil/impressionistpainter434/MainActivity.java
@@ -0,0 +1,301 @@
+package edu.umd.hcil.impressionistpainter434;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.ActionMenuView;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements OnMenuItemClickListener, SensorEventListener {
+
+    private static int RESULT_LOAD_IMAGE = 1;
+    private static int RESULT_CAMERA = 2;
+    private  ImpressionistView _impressionistView;
+    private SensorManager manager;
+    private Sensor accel;
+    private boolean tilt = false, image = false;
+
+    // These images are downloaded and added to the Android Gallery when the 'Download Images' button is clicked.
+    // This was super useful on the emulator where there are no images by default
+    private static String[] IMAGE_URLS ={
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/BoliviaBird_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/BolivianDoor_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/MinnesotaFlower_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/PeruHike_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/ReginaSquirrel_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/SucreDog_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/SucreStreet_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/SucreStreet_PhotoByJonFroehlich2(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/SucreWine_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/WashingtonStateFlower_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/JonILikeThisShirt_Medium.JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/JonUW_(853x1280).jpg",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/MattMThermography_Medium.jpg",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/PinkFlower_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/PinkFlower2_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/PurpleFlowerPlusButterfly_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/WhiteFlower_PhotoByJonFroehlich(Medium).JPG",
+            "http://www.cs.umd.edu/class/spring2016/cmsc434/assignments/IA08-AndroidII/Images/YellowFlower_PhotoByJonFroehlich(Medium).JPG",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _impressionistView = (ImpressionistView)findViewById(R.id.viewImpressionist);
+        ImageView imageView = (ImageView)findViewById(R.id.viewImage);
+        _impressionistView.setImageView(imageView);
+
+        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accel = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        manager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+    public void onButtonClickClear(View v) {
+        new AlertDialog.Builder(this)
+                .setTitle("Clear Painting?")
+                .setMessage("Do you really want to clear your painting?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Toast.makeText(MainActivity.this, "Painting cleared", Toast.LENGTH_SHORT).show();
+                        _impressionistView.clearPainting();
+                    }})
+                .setNegativeButton(android.R.string.no, null).show();
+    }
+
+    public void onButtonClickSetBrush(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuCircle:
+                Toast.makeText(this, "Circle Brush", Toast.LENGTH_SHORT).show();
+                _impressionistView.setBrushType(BrushType.Circle);
+                return true;
+            case R.id.menuSquare:
+                Toast.makeText(this, "Square Brush", Toast.LENGTH_SHORT).show();
+                _impressionistView.setBrushType(BrushType.Square);
+                return true;
+            case R.id.menuLine:
+                Toast.makeText(this, "Line Brush", Toast.LENGTH_SHORT).show();
+                _impressionistView.setBrushType(BrushType.Line);
+                return true;
+            case R.id.menuCircleSplatter:
+                Toast.makeText(this, "Circle Splatter Brush", Toast.LENGTH_SHORT).show();
+                _impressionistView.setBrushType(BrushType.CircleSplatter);
+                return true;
+            case R.id.menuLineSplatter:
+                Toast.makeText(this, "Line Splatter Brush", Toast.LENGTH_SHORT).show();
+                _impressionistView.setBrushType(BrushType.LineSplatter);
+                return true;
+            case R.id.menuEraser:
+                Toast.makeText(this, "Eraser", Toast.LENGTH_SHORT).show();
+                _impressionistView.setBrushType(BrushType.Eraser);
+                return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Downloads test images to use in the assignment. Feel free to use any images you want. I only made this
+     * as an easy way to get images onto the emulator.
+     *
+     * @param v
+     */
+    public void onButtonClickDownloadImages(View v){
+
+        // Without this call, the app was crashing in the onActivityResult method when trying to read from file system
+        FileUtils.verifyStoragePermissions(this);
+
+        // Amazing Stackoverflow post on downloading images: http://stackoverflow.com/questions/15549421/how-to-download-and-save-an-image-in-android
+        final BasicImageDownloader imageDownloader = new BasicImageDownloader(new BasicImageDownloader.OnImageLoaderListener() {
+
+            @Override
+            public void onError(String imageUrl, BasicImageDownloader.ImageError error) {
+                Log.v("BasicImageDownloader", "onError: " + error);
+            }
+
+            @Override
+            public void onProgressChange(String imageUrl, int percent) {
+                Log.v("BasicImageDownloader", "onProgressChange: " + percent);
+            }
+
+            @Override
+            public void onComplete(String imageUrl, Bitmap downloadedBitmap) {
+                File externalStorageDirFile = Environment.getExternalStorageDirectory();
+                String externalStorageDirStr = Environment.getExternalStorageDirectory().getAbsolutePath();
+                boolean checkStorage = FileUtils.checkPermissionToWriteToExternalStorage(MainActivity.this);
+                String guessedFilename = URLUtil.guessFileName(imageUrl, null, null);
+
+                // See: http://developer.android.com/training/basics/data-storage/files.html
+                // Get the directory for the user's public pictures directory.
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), guessedFilename);
+                try {
+                    boolean compressSucceeded = downloadedBitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(file));
+                    FileUtils.addImageToGallery(file.getAbsolutePath(), getApplicationContext());
+                    Toast.makeText(getApplicationContext(), "Saved to " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        for(String url: IMAGE_URLS){
+            imageDownloader.download(url, true);
+        }
+    }
+
+    /**
+     * Loads an image from the Gallery into the ImageView
+     *
+     * @param v
+     */
+    public void onButtonClickLoadImage(View v){
+
+        // Without this call, the app was crashing in the onActivityResult method when trying to read from file system
+        FileUtils.verifyStoragePermissions(this);
+
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+        image = true;
+    }
+
+    public void onButtonCameraImage(View v){
+
+        // Without this call, the app was crashing in the onActivityResult method when trying to read from file system
+        FileUtils.verifyStoragePermissions(this);
+
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, RESULT_CAMERA);
+        image = true;
+    }
+
+    /**
+     * Called automatically when an image has been selected in the Gallery
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imageView = (ImageView) findViewById(R.id.viewImage);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                // destroy the drawing cache to ensure that when a new image is loaded, its cached
+                imageView.destroyDrawingCache();
+                imageView.setImageBitmap(bitmap);
+                imageView.setDrawingCacheEnabled(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == RESULT_CAMERA && resultCode == RESULT_OK && null != data) {
+            try {
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                imageView.destroyDrawingCache();
+                imageView.setImageBitmap(bitmap);
+                imageView.setDrawingCacheEnabled(true);
+                imageView.setImageBitmap(bitmap);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onButtonClickSave(View v){
+        AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+        saveDialog.setTitle("Save drawing?");
+        saveDialog.setIcon(R.drawable.save2);
+        saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                saveImage();
+            }
+        });
+        saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                dialog.cancel();
+            }
+        });
+        saveDialog.show();
+    }
+
+    public void saveImage(){
+        //ImpressionistView impressionistView = (ImpressionistView) findViewById(R.id.viewImpressionist);
+        Bitmap bitmap = _impressionistView.get_offScreenBitmap();
+        String img = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, UUID.randomUUID().toString()+".png", "Impression");
+        String msg = img == null ? "Image failed to save" : "Drawing saved!";
+        Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_SHORT).show();
+        _impressionistView.destroyDrawingCache();
+    }
+
+    public void onToggle (View v) {
+        //ImpressionistView impressionistView = (ImpressionistView) findViewById(R.id.viewImpressionist);
+        _impressionistView.flipToggle();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float d = sensorEvent.values[0];
+        float r = sensorEvent.values[1];
+        //ImpressionistView impressionistView = (ImpressionistView) findViewById(R.id.viewImpressionist);
+
+        if (tilt && image) {
+            _impressionistView.sensorMove(r, d);
+            _impressionistView.invalidate();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        //blah
+    }
+
+    public void flipTilt(View v){
+        tilt = !tilt;
+    }
+
+    public void onClickErase(View v) {
+
+    }
+}
